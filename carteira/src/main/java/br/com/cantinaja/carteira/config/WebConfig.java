e56f99b99fe7c333eecea922302c39b62f92bcd3
Lucas Teles <lucasouzateles@gmail.com>
2026-07-15 23:47:46 -0300

    config: estruturando backend

---
+++ carteira/src/main/java/br/com/cantinaja/carteira/config/WebConfig.java
@@ -0,0 +1,19 @@
+package br.com.cantinaja.carteira.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Libera o CORS para o frontend consumir a API..
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+    }
+}
---
+++ pedidos/src/main/java/br/com/cantinaja/pedidos/PedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.cantinaja.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosApplication.class, args);
+	}
+
+}
